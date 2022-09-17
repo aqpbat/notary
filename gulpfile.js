64d87d9b64f8f396c02180e3b235e0c1314a13ba
mufassa <batyrakpanbet@gmail.com>
2022-09-18 01:56:09 +0600

    deleted comments in gulpjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,17 @@
-/**
-*   Gulp with TailwindCSS - An CSS Utility framework                                
-*   Author : Manjunath G                                              
-*   URL : manjumjn.com | lazymozek.com
-*   Twitter : twitter.com/manju_mjn                                    
-**/
-
-/*
-  Usage:
-  1. npm install //To install all dev dependencies of package
-  2. npm run dev //To start development and server for live preview
-  3. npm run prod //To generate minifed files for live server
-*/
-
 const { src, dest, task, watch, series, parallel } = require('gulp');
 const deleteAsync = require('del');
-const options = require("./config"); //paths and other options from config.js
+const options = require("./config");
 const browserSync = require('browser-sync').create();
 
-const sass = require('gulp-sass')(require('sass')); //For Compiling SASS files
-const postcss = require('gulp-postcss'); //For Compiling tailwind utilities with tailwind config
-const concat = require('gulp-concat'); //For Concatinating js,css files
-const uglify = require('gulp-terser');//To Minify JS files
-const cleanCSS = require('gulp-clean-css');//To Minify CSS files
-const purgecss = require('gulp-purgecss');// Remove Unused CSS from Styles
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
+const uglify = require('gulp-terser');
+const cleanCSS = require('gulp-clean-css');
+const purgecss = require('gulp-purgecss');
 const autoprefixer = require('gulp-autoprefixer');
-//Note : Webp still not supported in major browsers including forefox
-//const webp = require('gulp-webp'); //For converting images to WebP format
-//const replace = require('gulp-replace'); //For Replacing img formats to webp in html
 
-//Load Previews on Browser on dev
+
 function livePreview(done){
   browserSync.init({
     server: {
@@ -39,16 +22,17 @@ function livePreview(done){
   done();
 } 
 
-// Triggers Browser reload
+
 function previewReload(done){
   browserSync.reload();
   done();
 }
 
-//Development Tasks
+
 function devHTML(){
   return src(`${options.paths.src.base}/**/*.html`).pipe(dest(options.paths.dist.base));
 } 
+
 
 function devStyles(){
   const tailwindcss = require('tailwindcss'); 
@@ -66,6 +50,7 @@ function devStyles(){
     .pipe(dest(options.paths.dist.css));
 }
 
+
 function devScripts(){
   return src([
     `${options.paths.src.js}/libs/**/*.js`,
@@ -74,9 +59,11 @@ function devScripts(){
   ]).pipe(concat({ path: 'scripts.js'})).pipe(dest(options.paths.dist.js));
 }
 
+
 function devImages(){
   return src(`${options.paths.src.img}/**/*`).pipe(dest(options.paths.dist.img));
 }
+
 
 function watchFiles(){
   watch(`${options.paths.src.base}/**/*.html`,series(devHTML, devStyles, previewReload));
@@ -86,15 +73,17 @@ function watchFiles(){
   console.log("Watching for Changes...");
 }
 
+
 function devClean(){
   console.log("Cleaning dist folder for fresh start...");
   return deleteAsync(['dist']);
 }
 
-//Production Tasks (Optimized Build for Live/Production Sites)
+
 function prodHTML(){
   return src(`${options.paths.src.base}/**/*.html`).pipe(dest(options.paths.build.base));
 }
+
 
 function prodStyles(){
   return src(`${options.paths.dist.css}/**/*`)
@@ -110,6 +99,7 @@ function prodStyles(){
   .pipe(dest(options.paths.build.css));
 }
 
+
 function prodScripts(){
   return src([
     `${options.paths.src.js}/libs/**/*.js`,
@@ -120,25 +110,29 @@ function prodScripts(){
   .pipe(dest(options.paths.build.js));
 }
 
+
 function prodClean(){
   console.log("Cleaning build folder for fresh start...");
-  return deleteAsync(['dist']);
+  return deleteAsync(['build']);
 }
+
 
 function buildFinish(done){
   console.log(`Production build is complete. Files are located at ${options.paths.build.base}...`);
   done();
 }
 
+
 exports.default = series(
-  devClean, // Clean Dist Folder
-  parallel(devStyles, devScripts, devImages, devHTML), //Run All tasks in parallel
-  livePreview, // Live Preview Build
-  watchFiles // Watch for Live Changes
+  devClean,
+  parallel(devStyles, devScripts, devImages, devHTML),
+  livePreview,
+  watchFiles
 );
 
+
 exports.prod = series(
-  prodClean, // Clean Build Folder
-  parallel(prodStyles, prodScripts, prodHTML), //Run All tasks in parallel
+  prodClean,
+  parallel(prodStyles, prodScripts, prodHTML),
   buildFinish
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mode: 'jit', // Just-In-Time Compiler
+  mode: 'jit',
   purge: ['./src/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {},
   },
