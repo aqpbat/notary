437c26e08dcbd64712036fa989b05ebfa3275bb8
mfsa <batyrakpanbet@gmail.com>
2022-09-18 20:00:24 +0600

    working with header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-//main.js file
+function showMenu() {
+  let menuShowButton = document.querySelector('#menuShowButton')
+  let mobileMenu = document.querySelector('#mobileMenu')
+
+  menuShowButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+  })
+}
+
+showMenu()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.html'],
-  darkMode: false,
+  darkMode: true,
   theme: {
     extend: {},
+    screens: {
+      'lg': {'max': '992px'},
+      'md': {'max':'768px'},
+      'sm': {'max': '480px'}
+    },
+    container: {
+      padding: '20px',
+      center: true,
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
